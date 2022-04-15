eaf7820a89331f48de6dc30e62d0d0ff142d3660
volodymyr-gh <volodymyr.sirenko@gmail.com>
2022-04-15 15:35:49 +0300

    Add test cases for non-authorized access and invalid request body; Mix in TestDBProvider and AuthenticationProvider

--- test/dev/volodymyr/test/task/endpoints/CreateTaskSpec.scala
+++ test/dev/volodymyr/test/task/endpoints/CreateTaskSpec.scala
@@ -1,27 +1,54 @@
 package dev.volodymyr.test.task.endpoints
 
+import dev.volodymyr.testUtils.{AuthenticationProvider, TestDBProvider}
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice.GuiceOneServerPerSuite
 import play.api.libs.json.Json
-import play.api.test._
 import play.api.test.Helpers._
 import play.api.libs.ws.WSClient
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration.Inf
 
-class CreateTaskSpec extends PlaySpec with GuiceOneServerPerSuite {
-  private def wsClient: WSClient = app.injector.instanceOf[WSClient]
+class CreateTaskSpec
+  extends PlaySpec
+    with GuiceOneServerPerSuite
+    with TestDBProvider
+    with AuthenticationProvider {
 
-  private def url: String = s"http://localhost:$port/tasks"
+  private def wsClient = app.injector.instanceOf[WSClient]
+
+  private def url = s"http://localhost:$port/tasks"
 
   "POST request to /tasks endpoint" should {
     "return status 201 and newly created task in the body" in {
-      val token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NTI1ODA3NDEsInV1aWQiOiJkNTBlMTQyNS02OTM0LTQ0YzYtOWRmNi00OWJiODFkYTFiNDkiLCJlbWFpbCI6ImpvaG5AZXhhbXBsZS5jb20ifQ.9vmnbPKfINWQbC6Ow7UVV3ncwQ0M34yQMm_hWjfKaRQ"
+      val currentUser = Await.result(createFixtureUser(), Inf)
 
       val req = wsClient.url(url)
-        .withHttpHeaders(("Authorization", s"Bearer $token"))
+        .withHttpHeaders(buildAuthHeaders(currentUser): _*)
         .post(Json.obj("summary" -> "do something"))
 
       val res = await(req)
       res.status mustBe CREATED
+      // TODO check body
+    }
+
+    "return status 401 if the request is not authenticated" in {
+      val req = wsClient.url(url)
+        .post(Json.obj("summary" -> "do something"))
+
+      val res = await(req)
+      res.status mustBe UNAUTHORIZED
+    }
+
+    "return status 400 if the request body is missing summary" in {
+      val currentUser = Await.result(createFixtureUser(), Inf)
+
+      val req = wsClient.url(url)
+        .withHttpHeaders(buildAuthHeaders(currentUser): _*)
+        .post(Json.obj())
+
+      val res = await(req)
+      res.status mustBe BAD_REQUEST
     }
   }
 }
